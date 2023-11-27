1221bb4a24b1634ad904a775c83073f2f62422e3
lim-sorry <58265373+lim-sorry@users.noreply.github.com>
2023-11-27 15:05:48 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,35 +5,37 @@ function generateDictionary() {
     var name = nameInput.value;
     var code = codeInput.value;
 
-    var namesArray = [["지성","경수"], ["덕용","영현"], ["재찬","나연"], ["재윤","이슬"], ["미정","준철"], ["민석","민승"]];
+    var namesArray = [["지성"], ["경수"], ["주현"], ["덕용","영현"], ["재찬","나연"], ["재윤","이슬"], ["미정","준철"], ["민석","민승"]];
     
     if (!name || !code) {
         alert('Please enter both name and code.');
         return;
     }
 
-    var obfuscationKey = 20;
-    var cArray = new Array(39, 19);
-    cArray.push(126, 106);
-    cArray = cArray.concat(77, 97);
+    var obfuscationKey = 167;
+    var cArray = [57152,81238,15784,45843,34561,94255,18421,67813,35464]
     var dArray = cArray.map(function (code) {
-        return code ^ obfuscationKey;
+        var key = code * obfuscationKey ^ obfuscationKey;
+        while (key > 10000) {
+            key = Math.round(key/10);
+        }
+        return key;
     });
     
     Math.seedrandom(code);
-    for (var i=dArray.length-1; i>-1; i--){
-        if (namesArray[i][0] == name || namesArray[i][1] == name){
-            //console.log(name);
-            if (code == dArray[i]){
-                var sd = dArray.reduce((a,b)=>a+b, 0);
-                Math.seedrandom(sd);
-                //console.log(sd);
-                resultExtract(namesArray, name);
-            } else {
-                alert('Please enter right code for name.');
+    for (var i=namesArray.length-1; i>-1; i--){
+        namesArray[i].forEach(elem => {
+            if(elem == name){
+                if (code == dArray[i]){
+                    var sd = dArray.reduce((a,b)=>a+b, 0);
+                    Math.seedrandom(sd);
+                    //console.log(sd);
+                    resultExtract(namesArray, name);
+                } else {
+                    alert('Please enter right code for name.');
+                }
             }
-            break;
-        }
+        });
     }    
 
 }
@@ -55,34 +57,51 @@ function shuffleArray(array) {
 function resultExtract(namesArray, name) {
     var resultDiv = document.getElementById('result');
 
+    var name2;
     for (var i=0; i<namesArray.length; i++) {
-        if (name == namesArray[i][0]) var name2 = namesArray[i][1];
-        else if (name == namesArray[i][1]) var name2 = namesArray[i][0];
-    }    
+        if (namesArray[i].length < 2) continue;
+        else if (name == namesArray[i][0]) name2 = namesArray[i][1];
+        else if (name == namesArray[i][1]) name2 = namesArray[i][0];
+    }
 
-    var names1 = [];
-    var names2 = [];
-    for (var i=0; i<namesArray.length-1; i++) {
+    var shuffledNames1 = [];
+    var shuffledNames2 = [];
+    for (var i=4; i<namesArray.length; i++) {
         if (Math.random() > 0.5) {
-            names1[i] = namesArray[i][0];
-            names2[i] = namesArray[i][1];
+            shuffledNames1[i-4] = namesArray[i][0];
+            shuffledNames2[i-4] = namesArray[i][1];
         }else{
-            names1[i] = namesArray[i][1];
-            names2[i] = namesArray[i][0];
+            shuffledNames1[i-4] = namesArray[i][1];
+            shuffledNames2[i-4] = namesArray[i][0];
         }
     }
-    names1 = shuffleArray(names1);
-    names2 = shuffleArray(names2);
+    shuffledNames1 = shuffleArray(shuffledNames1);
+    shuffledNames2 = shuffleArray(shuffledNames2);
     
+    shuffledNames1.push(namesArray[3][0]);
+    shuffledNames2.push(namesArray[3][1]);
 
-    names1.push(namesArray[namesArray.length-1][0]);
-    names2.push(namesArray[namesArray.length-1][1]);
-    var names = names1.concat(names2, names1[0]);
-    //console.log(names);
-
-    var manito = names[names.findIndex((elem) => elem == name) + 1];
-    var manito2 = names[names.findIndex((elem) => elem == name2) + 1];
+    var shuffledNames = shuffledNames1.concat(shuffledNames2);
     
-    // Display the result
-    resultDiv.innerHTML = `<p>${name} > ${manito}\t&\t${name2} > ${manito2}</p>`;
+    for(var i=0; i<3; i++) {
+        insertElementAtRandomIndex(shuffledNames, namesArray[i][0]);
+    }
+    shuffledNames.push(shuffledNames[0]);
+    //console.log(shuffledNames);
+    
+    var manito = shuffledNames[shuffledNames.findIndex((elem) => elem == name) + 1];
+    if (!name2){
+        resultDiv.innerHTML = `<p>${name} > ${manito}</p>`;
+    } else {
+        var manito2 = shuffledNames[shuffledNames.findIndex((elem) => elem == name2) + 1];    
+        resultDiv.innerHTML = `<p>${name} > ${manito}\t&\t${name2} > ${manito2}</p>`;
+    }
+}
+
+function insertElementAtRandomIndex(array, element) {
+    // Get a random index within the current array length
+    var randomIndex = Math.floor(Math.random() * (array.length + 1));
+
+    // Insert the element at the random index
+    array.splice(randomIndex, 0, element);
 }
